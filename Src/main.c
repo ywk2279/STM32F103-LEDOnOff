acdddf4bdf2254b38e1347dee7b21769a69108bd
YoungWoong <ywk2279@naver.com>
2019-04-30 11:24:27 +0900

    switch 기능을 추가하여 sw1은 300ms, sw2는 100ms로 동작하도록 기능 추가

--- Src/main.c
+++ Src/main.c
@@ -99,10 +99,19 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  int delayValue = 0;
   while (1)
   {
+	  if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_0) == 0)
+	  {
+		  delayValue = 300;
+	  }
+	  else if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_1) == 0)
+	  {
+		  delayValue = 100;
+	  }
 	  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
-	  HAL_Delay(200);
+	  HAL_Delay(delayValue);
   /* USER CODE END WHILE */
 
   /* USER CODE BEGIN 3 */
@@ -177,6 +186,7 @@ static void MX_GPIO_Init(void)
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
@@ -188,6 +198,12 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PB0 PB1 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
 
